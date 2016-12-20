1ab234e8f4302d0ba1b7e33caf724790030eec6d
Jordan Riser <riser.jordan@gmail.com>
2016-12-20 14:47:08 -0500

    added rank icon computed property

--- app/models/overwatch/competetive/stat.js
+++ app/models/overwatch/competetive/stat.js
@@ -1,8 +1,28 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   startingSR: DS.attr('number'),
   currentSR: DS.attr('number'),
   gamesWon: DS.attr('number'),
-  gamesLost: DS.attr('number')
+  gamesLost: DS.attr('number'),
+
+  rankClass: Ember.computed('currentSR',function(){
+    var sr = this.get('currentSR');
+    if(sr >= 0 && sr <= 1499){
+      return 'bronze';
+    }else if(sr >= 1500 && sr <= 1999){
+      return 'silver';
+    }else if(sr >= 2000 && sr <= 2499){
+      return 'gold';
+    }else if(sr >= 2500 && sr <= 2999){
+      return 'platinum';
+    }else if(sr >= 3000 && sr <= 3499){
+      return 'diamond';
+    }else if(sr >= 3500 && sr <= 3999){
+      return 'master';
+    }else if(sr >= 4000 && sr <= 5000){
+      return 'grand-master';
+    }
+  }),
 });
