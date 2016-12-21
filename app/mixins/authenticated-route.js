9efd8db15c37bfc78c0b86394d92a500c0dddd67
Jordan Riser <riser.jordan@gmail.com>
2016-12-21 09:48:04 -0500

    injecting location service and setting attempted transition

--- app/mixins/authenticated-route.js
+++ app/mixins/authenticated-route.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
-  beforeModel: function() {
+  location: Ember.inject.service(),
+  beforeModel: function(transition) {
     var self = this;
     this.get('session').fetch().catch(function() {
+      self.set('location.attemptedTransition',transition);
       self.transitionTo('login');
     });
   },
