05df011e74e2c8c12f97a74fe9226aefa8365c15
Jordan Riser <riser.jordan@gmail.com>
2016-12-21 10:11:44 -0500

    removing console log

--- app/controllers/overwatch/competetive/input.js
+++ app/controllers/overwatch/competetive/input.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   startingSRObserverFired: 0,
   startingSRObserver: Ember.observer('model.{startingSR}', function() {
-    console.log('beep');
     this.incrementProperty('startingSRObserverFired');
     if(this.get('startingSRObserverFired') > 1){
       this.set('model.currentSR',this.get('model.startingSR'));
