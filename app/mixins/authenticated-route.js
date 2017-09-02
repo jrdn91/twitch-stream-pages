1128ea45db968503de011edacba00358699289da
Jordan Riser <riser.jordan@gmail.com>
2017-09-02 11:30:17 -0400

    fix all issues

--- app/mixins/authenticated-route.js
+++ app/mixins/authenticated-route.js
@@ -4,6 +4,9 @@ export default Ember.Mixin.create({
   location: Ember.inject.service(),
   beforeModel: function(transition) {
     var self = this;
+    if (this.session.get('isAuthenticated')) {
+      return; // Already authenticated
+    }
     this.get('session').fetch().catch(function() {
       self.set('location.attemptedTransition',transition);
       self.transitionTo('login');
