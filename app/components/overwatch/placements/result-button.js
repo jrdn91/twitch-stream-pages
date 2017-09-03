64a73473b0a738a19760237c54a0fe680c44f620
Jordan Riser <riser.jordan@gmail.com>
2017-09-03 18:02:44 -0400

    adding win and loss buttons

--- app/components/overwatch/placements/result-button.js
+++ app/components/overwatch/placements/result-button.js
@@ -2,5 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: 'result-button',
-  classNameBindings: ['result.winStateClass']
+  classNameBindings: ['result.winStateClass'],
+
+  actions: {
+    clickAction(){
+      this.set('showButtons',true);
+      Ember.run.later(()=>{
+        this.set('showButtons',false);
+      },5000);
+    }
+  }
 });
