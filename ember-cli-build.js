9bf6ca31e89377e52c82bc1dc72fec553d232295
Jordan Riser <riser.jordan@gmail.com>
2017-09-04 10:00:57 -0400

    add ember math helpers

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,6 +8,9 @@ module.exports = function(defaults) {
       includePaths: [
         'bower_components/bootstrap-sass/assets/stylesheets'
       ]
+    },
+    'ember-math-helpers': {
+      only: ['add']
     }
   });
 
